e5a3446102256e95da6f9328b01006a13c3aed9e
rlvrs <rodriisantos@gmail.com>
2020-11-19 02:13:26 +0000

    Initial commit project structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# dummy-aws-ssh-server-authorizer
+# Dummy AWS SSH Server manager
+This is a dummy implementation of the same service in multiple languages and frameworks.
+This repo holds opinionated code that aims to compare different environments.
---
+++ spring-kotlin/aws-ssh-server-manager/src/main/kotlin/dev/santos/awssshservermanager/AwsSshServerManagerApplication.kt
@@ -0,0 +1,11 @@
+package dev.santos.awssshservermanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsSshServerManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwsSshServerManagerApplication>(*args)
+}
